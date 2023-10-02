b3dd1ef08e9f57e7d4439dfd68140f6778364c2f
avnesas <112384357+avnesas@users.noreply.github.com>
2023-10-02 20:09:13 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,27 +3,23 @@ function dynamicTabTitle() {
   const titleElement = document.getElementById("dynamic-title");
   const text = "THE LINK_";
   let index = 0;
-  let isBlinking = true;
+  let isTyping = true;
 
   function updateTitle() {
-    titleElement.textContent = text.substring(0, index);
-    if (index === text.length) {
-      setTimeout(reverseTitle, 4000); // Wait for 4 seconds before reversing
-    } else {
+    if (isTyping) {
+      titleElement.textContent = text.substring(0, index);
+      if (index === text.length) {
+        isTyping = false;
+        setTimeout(updateTitle, 4000); // Wait for 4 seconds before reversing
+      }
       index++;
-      setTimeout(updateTitle, 200); // Typing speed
-    }
-  }
-
-  function reverseTitle() {
-    if (index === 0) {
-      index++;
-      isBlinking = true;
-      setTimeout(updateTitle, 200); // Typing speed
     } else {
-      titleElement.textContent = text.substring(0, text.length - index) + "_";
+      titleElement.textContent = text.substring(0, index);
+      if (index === 0) {
+        isTyping = true;
+        setTimeout(updateTitle, 0); // Start typing immediately
+      }
       index--;
-      setTimeout(reverseTitle, 100); // Reversing speed
     }
   }
 
