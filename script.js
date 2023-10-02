33de6bf4b25c5da36dd1046b136e1070cc36423e
avnesas <112384357+avnesas@users.noreply.github.com>
2023-10-02 20:05:48 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,32 @@
 // script.js
 function dynamicTabTitle() {
   const titleElement = document.getElementById("dynamic-title");
-  const text = "THE LINK";
+  const text = "THE LINK_";
   let index = 0;
-  let blinkCount = 0;
+  let isBlinking = true;
 
   function updateTitle() {
-    titleElement.textContent = text.substring(0, index) + (blinkCount % 2 === 0 ? "_" : "");
+    titleElement.textContent = text.substring(0, index);
     if (index === text.length) {
-      blinkCount++;
-      if (blinkCount === 8) { // Blink four times and then stop
-        setTimeout(reverseTitle, 4000); // Pause before reversing (4 seconds)
-      } else {
-        setTimeout(updateBlink, 4000); // Blink every 4 seconds
-      }
+      setTimeout(reverseTitle, 4000); // Wait for 4 seconds before reversing
     } else {
       index++;
-      setTimeout(updateTitle, 100); // Typing speed
+      setTimeout(updateTitle, 200); // Typing speed
     }
-  }
-
-  function updateBlink() {
-    titleElement.textContent = text + "_";
-    setTimeout(updateTitle, 4000); // Pause before typing (4 seconds)
   }
 
   function reverseTitle() {
-    index = text.length;
-    setTimeout(updateReverse, 4000); // Backspace speed (4 seconds)
-  }
-
-  function updateReverse() {
-    titleElement.textContent = text.substring(0, index) + "_";
     if (index === 0) {
-      index = text.length;
-      setTimeout(updateTitle, 4000); // Pause before typing (4 seconds)
+      index++;
+      isBlinking = true;
+      setTimeout(updateTitle, 200); // Typing speed
     } else {
+      titleElement.textContent = text.substring(0, text.length - index) + "_";
       index--;
-      setTimeout(updateReverse, 100); // Backspace speed (typing speed)
+      setTimeout(reverseTitle, 100); // Reversing speed
     }
   }
 
-  // Initial call to start the animation
   updateTitle();
 }
 
